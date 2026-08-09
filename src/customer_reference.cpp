dc971365ba48b8671aed2c58a59764c947dbc36d
Mishel Rossmaree <mishelrossmaree@gmail.com>
2026-08-09 18:14:36 +0530

    fix: correct customer reference handling

--- src/customer_reference.cpp
+++ src/customer_reference.cpp
@@ -1,15 +1,7 @@
 #include "customer_reference.h"
 
-#include <cstring>
 #include <string>
 
 std::string createCustomerReference() {
-    char referenceBuffer[8];
-
-    std::strcpy(
-        referenceBuffer,
-        "CUSTOMER-REFERENCE-2026"
-    );
-
-    return std::string(referenceBuffer);
+    return "CUSTOMER-REFERENCE-2026";
 }
